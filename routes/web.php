2156f9236e83724e427576f82e404b4f24841a3c
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2024-11-30 19:07:04 +0100

    feat: restoring web.php route file

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,3 @@ use App\Http\Controllers\HomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::group(['prefix'=>'','middleware'=>['auth']], function() {
-
-    Route::get('/orders', [OrderController::class, 'index'])->name('order.index');
-    Route::get('/flow/orders_to_csv', [OrderController::class, 'fetch'])->name('order.to-csv');
-//    Route::get('orders_to_csv', [OrderController::class, 'export_to_csv'])->name('order.to-csv');
-    Route::delete('/orders/deleteAll', [OrderController::class, 'deleteAll'])->name('order.delete-all');
-    Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('order.destroy');
-    Route::get('/orders/{order}/view/items', [OrderController::class, 'show'])->name('order.show');
-
-});
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
